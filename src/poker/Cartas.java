bf720e48c7f0c2968cd72f1b1e53e94ebf71ef61
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2019-08-05 13:46:25 -0500

    null

---
+++ src/poker/Cartas.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poker;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+
+/**
+ *
+ * @author crhistian
+ */
+public class Cartas {
+    /**Cara trasera de la carta*/
+    public static ImageIcon back;
+    /**Cara delantera de la carta*/
+    private ImageIcon front;
+    
+    public Cartas(ImageIcon image){
+    	this.back = back;
+    	this.front = image;
+    	setIcon(back);
+    	
+    }
+}
---
+++ src/poker/Control.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poker;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author crhistian
+ */
+public class Control {
+	
+    private ArrayList<Cartas> baraja, pila;
+    
+    public void control(){
+        baraja = new ArrayList<Cartas> (52);
+    }
+    
+}
